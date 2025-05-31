6cf742aa0ec9989cc0adb709136b67405a93fb30
furkanturkn <furkan.trkn1@gmail.com>
2025-05-31 14:02:49 +0300

    Integrate DevengNetworkingModule
    This commit integrates the `DevengNetworkingModule` into `PinqponqChatModule.kt`.
    
    - Adds `deveng-networking-kmp` as a dependency in `gradle/libs.versions.toml` and `pinqponqchat/build.gradle.kts`.
    - Initializes `DevengNetworkingModule` in the `test()` function of `PinqponqChatModule.kt`.
    - Makes a sample GET request to "v1/chat/messages" within a coroutine in the `test()` function.

--- pinqponqchat/build.gradle.kts
+++ pinqponqchat/build.gradle.kts
@@ -67,6 +67,7 @@ kotlin {
 
         commonMain.dependencies {
             implementation(compose.material3)
+            implementation(libs.deveng.networking.kmp)
 
         }
 
--- pinqponqchat/src/commonMain/kotlin/PinqponqChatModule.kt
+++ pinqponqchat/src/commonMain/kotlin/PinqponqChatModule.kt
@@ -1,9 +1,35 @@
 package pinqponqchat
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import networking.DevengNetworkingConfig
+import networking.DevengNetworkingModule
+import networking.util.DevengHttpMethod
+
 public object PinqponqChatModule {
 
-    public fun test(){
+    public fun test() {
         println("test")
+        DevengNetworkingModule.initDevengNetworkingModule(
+            restBaseUrl = "https://api.pinqponq.io",
+            config = DevengNetworkingConfig(
+                loggingEnabled = true
+            )
+        )
+        GlobalScope.launch {
+            try {
+
+                val a = DevengNetworkingModule.sendRequest<Unit, Unit>(
+                    endpoint = "v1/chat/messages",
+                    requestMethod = DevengHttpMethod.GET,
+
+                    )
+
+
+            } catch (e: Exception) {
+                println("Error: ${e.message}")
+            }
+        }
     }
 
 }
